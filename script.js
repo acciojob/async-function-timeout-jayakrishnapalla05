b1a8208ddea83953e7f794b9580bd5d8e2ced921
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-07-05 14:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const submitButton = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+
+async function showMessageWithDelay() {
+  const text = textInput.value;
+  const delay = parseInt(delayInput.value, 10);
+  await new Promise(resolve => setTimeout(resolve, delay));
+  outputDiv.innerText = text;
+}
+submitButton.addEventListener('click', showMessageWithDelay);
